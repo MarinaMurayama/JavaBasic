2a041a2350558ef2cacf16869889b022a2c7e231
MurayamaMarina <marina.anzu.coco.mame@gmail.com>
2018-03-22 14:35:13 +0900

    PTra01_07

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,23 +21,25 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
+		bo = false;
+		by = 2;
+		s = 3;
+		c = '村';
+		i = 5;
+		l = 123456789;
+		f = 7.3F;
+		d = 8.123;
 
 
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
+		 System.out.println(bo);
+		 System.out.println(by);
+		 System.out.println(s);
+		 System.out.println(c);
+		 System.out.println(i);
+		 System.out.println(l);
+		 System.out.println(f);
+		 System.out.println(d);
 
 
 	}
