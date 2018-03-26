2c167621301eb5d1a8a9ddc8cca8d56763e090df
MurayamaMarina <marina.anzu.coco.mame@gmail.com>
2018-03-26 15:00:46 +0900

    PTra06-07

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,26 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
+			for(int i = 1; i <= 6; i++){
+				for(int j = 1;j <= 5; j++){
+					if(i <= j){
+						System.out.print("□");
+					}else{
+						System.out.print("■");
+					}
+				}
+				System.out.println();
+				}
+
+			for(int i = 1; i <= 4; i++){
+				for(int j = 1; j <= 5; j++){
+					if( j <= 5-i ){
+						System.out.print("■");
+					}else{
+						System.out.print("□");
+					}
+				}
+				System.out.println();
+			}
+		}
 	}
-}
