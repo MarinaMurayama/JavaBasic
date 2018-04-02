3b17c013baa03604377cca7b10c7d1a22cfb1b7a
DESKTOP-9CLUSVJ\marin <marina.anzu.coco.mame@gmail.com>
2018-04-02 10:42:07 +0900

    PTra11-02

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -11,10 +11,22 @@ public class PTra11_02 {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
 
-
-		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		 Book[] bookList = FileReaderClass.readBookDataFile();
+		 System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
 
+		 java.util.Scanner scanner = new java.util.Scanner(System.in);
+		 String line = scanner.nextLine();
+
+
+		 for(int i = 0;  i< bookList.length; i++) {
+
+		 if (bookList[i].title.indexOf(line) != -1) {
+
+		 System.out.println(bookList[i].dispBookInfo());
+		 }
+		 }
 	}
 }
+
