0644fa83bdd4c3d0186f593f3d3658a3c4ff95f5
DESKTOP-9CLUSVJ\marin <marina.anzu.coco.mame@gmail.com>
2018-04-05 18:10:30 +0900

    PTra16-01 ~ 16-02

--- Practice/src/practice16/PTra16_02.java
+++ Practice/src/practice16/PTra16_02.java
@@ -10,14 +10,30 @@ public class PTra16_02 {
 	public static void main(String[] args) {
 
 		boolean b = true;
+		Boolean z = new Boolean(b);
+
 		byte by = 100;
+		Byte y = new Byte(by);
+
 		short s = 300;
+		Short x = new Short(s);
+
 		char c = '日';
+		Character u = new Character(c);
+
 		int i = 1000;
+		Integer t =new Integer(i);
+
 		long l = 2000L;
+		Long ab = new Long(l);
+
 		float f = 3.14F;
+		Float r = new Float(f);
+
 		double d = 453.592;
+		Double q = new Double(d);
 
 		// ★ 上記基本型を、それぞれのラッパークラスに代入してください
+
 	}
 }
