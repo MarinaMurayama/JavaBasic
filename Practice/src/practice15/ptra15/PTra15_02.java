e38e75d3faae0ae210cfea5d1d1e4cbf8a4f9293
DESKTOP-9CLUSVJ\marin <marina.anzu.coco.mame@gmail.com>
2018-04-05 14:45:27 +0900

    PTra15-01 ~ 15-02

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -12,12 +12,17 @@ import practice15.common.Room;
 public class PTra15_02 {
 	public static void main(String[] args) {
 
-		Room room = new Java2Month();
+		Room room = new Java2Month();     //3ページ目参照
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
 
+		Java2Month jroom = (Java2Month)room;//←☆キャスト☆  String[]list = room.getCurriculum();ではエラーになるので
+		String[]list = jroom.getCurriculum();//roomからJava2Monthへクラスを強制変換。
 
 		// 取得したカリキュラム情報を全て出力してください
 
+		for(String curriculum : list ) {
+		System.out.println(curriculum);
+		}
 	}
 }
