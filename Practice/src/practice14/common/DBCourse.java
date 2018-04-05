6b6f30d446470cfcad787082a36f4c096ecf584c
DESKTOP-9CLUSVJ\marin <marina.anzu.coco.mame@gmail.com>
2018-04-05 10:31:27 +0900

    PTra14-01 ~ 14-02

---
+++ Practice/src/practice14/common/DBCourse.java
@@ -0,0 +1,13 @@
+package practice14.common;
+
+public class DBCourse implements Course{
+
+	public String getCourseName() {
+		return "【Eラーニング】DB基礎";
+	}
+
+	public String[] getCourseUnit() {
+		return  new String[] {"DB基礎","SQL基礎","正規化","SQL応用"};   //配列はnewつけるのを忘れない。
+	}
+
+}
---
+++ Practice/src/practice14/common/JavaCourse.java
@@ -0,0 +1,12 @@
+package practice14.common;
+
+public  class JavaCourse implements Course{
+
+	public String getCourseName() {
+		return "【Eラーニング】Java";
+	}
+
+	public String[] getCourseUnit() {
+		return  new String[] {"式と演算","制御構文","メソッド","配列","オブジェクト指向","継承","高度な継承"};
+	}
+}
