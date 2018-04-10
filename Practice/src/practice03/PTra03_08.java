1fa2d882e1ffd7dad0f55803173adf949aee98e4
DESKTOP-9CLUSVJ\marin <marina.anzu.coco.mame@gmail.com>
2018-04-11 08:39:06 +0900

    PTra03-04 ~ 03-09 修正

--- Practice/src/practice03/PTra03_08.java
+++ Practice/src/practice03/PTra03_08.java
@@ -36,22 +36,31 @@ public class PTra03_08 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 
-		int orange = 150 ;
-		int apple = 160 ;
-		int coffee = 200 ;
-				
-		
-		switch(num) {
-		case 1:
-			System.out.println(money - orange);
-		    break;
-		case 2:
-			System.out.println(money - apple);
-		    break;
-		case 3:
-			System.out.println(money - coffee);
-		   break;
-		 
+		/*
+		 * int orange = 150 ;
+		 *int apple = 160 ;
+		 *int coffee = 200 ;
+		 *
+		 *switch(num) {
+		 *case 1:
+		 *System.out.println(money - orange);
+		 *break;
+		 *case 2:
+		 *System.out.println(money - apple);
+		 *break;
+		 *case 3:
+		 *System.out.println(money - coffee);
+		 *break;
+		 */
+
+		if (num == 1) {
+			money = money - 150;
+		} else if (num == 2) {
+			money = money - 160;
+		} else if (num == 3) {
+			money = money - 200;
+		}
+		System.out.println("所持金：" + money);
 	}
 }
-}
+
